df3fabaa250fded5fc19748d3f5213c3cdd3c960
brianail <80833852+brianail@users.noreply.github.com>
2022-04-18 21:23:11 -0300

    V7

--- index.js
+++ index.js
@@ -113,6 +113,32 @@ ScrollReveal().reveal('.skils', {
     interval: 400
 });
 
+ScrollReveal().reveal('.card-mobile', {
+  distance: '20%',
+  delay: 200,
+  origin: 'left',
+  interval: 400
+});
+
+ScrollReveal().reveal('.carrosel', {
+  distance: '100%',
+  delay: 200,
+  origin: 'left',
+  interval: 200
+});
+
+ScrollReveal().reveal('.app-conteudo', {
+  distance: '20%',
+  delay: 700,
+  origin: 'left',
+});
+
+ScrollReveal().reveal('.see-github', {
+  distance: '20%',
+  delay: 1000,
+  origin: 'bottom',
+});
+
 
 // Codigo para enviar os forms para o Netify
 document
@@ -134,62 +160,3 @@ const handleSubmit = (e) => {
 
 
 
-
-var position = document.querySelector('.position')
-  var quant = document.querySelectorAll('.item-slide .images')
-  var atual = 0
-  var imagem = document.getElementById('atual')
-  var avancar = document.getElementById('avancar')
-  var voltar = document.getElementById('voltar')
-  var rolar = true
-
-  for (let i = 0; i < quant.length; i++) {
-    var div = document.createElement('div')
-    div.id = i
-    position.appendChild(div)
-  }
-  document.getElementById('0').classList.add('primeira')
-
-  var pos = document.querySelectorAll('.position div')
-
-  for (let i = 0; i < pos.length; i++) {
-    pos[i].addEventListener('click', () => {
-      atual = pos[i].id
-      rolar = false
-      slide()
-    })
-  }
-
-  voltar.addEventListener('click', () => {
-    atual--
-    rolar = false
-    slide()
-  })
-
-  avancar.addEventListener('click', () => {
-    atual++
-    rolar = false
-    slide()
-  })
-
-  function slide() {
-    if (atual >= quant.length) {
-      atual = 0;
-    } else if (atual < 0) {
-      atual = quant.length - 1
-    }
-
-    document.querySelector('.primeira').classList.remove('primeira')
-    imagem.style.marginLeft = -1024 * atual +'px'
-    document.getElementById(atual).classList.add('primeira')
-
-  }
-
-  setInterval(()=>{
-    if(rolar){
-      atual++
-      slide()
-    }else{
-      rolar = true
-    }
-  },4000)
